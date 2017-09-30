5282bed101618c520525f7669325e3c2569ab75d
Jiří Janoušek <janousek.jiri@gmail.com>
2017-09-30 09:31:51 +0200

    Warn that only amd64 builds are provided
    But only if amd64 processor is not detected.
    
    Issue: tiliado/nuvola-repository-index#2
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- static/js/app.js
+++ static/js/app.js
@@ -41,4 +41,31 @@
   }
 
   checkHash()
+  var PLATFORM_X64 = ['x86_64', 'x86-64'];
+  var USER_AGENT_X64 = [
+    'x86_64', 'x86-64', 'Win64', 'x64;', 'amd64', 'AMD64', 'WOW64', 'x64_64'
+  ]
+
+  var isPlatformAmd64 = function () {
+    var ua = navigator.userAgent;
+    var oscpu = window.navigator.oscpu || "";
+    var platform = window.navigator.platform || "";
+    for (var i = 0; i < PLATFORM_X64.length; i++) {
+      var test = PLATFORM_X64[i];
+      if (platform.indexOf(test) !== -1 || oscpu.indexOf(test) !== -1) {
+        return true;
+      }
+    }
+    for (i = 0; i < USER_AGENT_X64.length; i++) {
+      if (ua.indexOf(USER_AGENT_X64[i]) !== -1) {
+        return true;
+      }
+    }
+    return false;
+  }
+  var notAmd64 = !isPlatformAmd64();
+  var elms = window.document.querySelectorAll('.not-amd64')
+  for (var i = 0; i < elms.length; i++) {
+    elms[i].style.display = notAmd64 ? "block" : "none"
+  }
 })(window)
