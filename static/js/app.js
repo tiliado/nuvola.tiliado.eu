7ae446164baa3fe72494199501616f9eed2bd088
Jiří Janoušek <janousek.jiri@gmail.com>
2018-02-12 18:58:42 +0100

    Use standardjs
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 (function (window) {
-
   var parseHash = function (hash) {
     var parts = (hash || '').split('!')
     var result = {}
@@ -17,55 +16,57 @@
     }
     var distro = hash.distro
     var release = hash.release
-    var distro_spec = ''
+    var distroSpec = ''
     if (distro) {
-      distro_spec = '/' + distro
+      distroSpec = '/' + distro
       if (release) {
-        distro_spec += '/' + release
+        distroSpec += '/' + release
       }
     }
 
-    var url;
+    var url
     switch (target || 'index') {
       case 'index':
-        url = distro_spec ? '/index' + distro_spec + '/' : '/'
+        url = distroSpec ? '/index' + distroSpec + '/' : '/'
         break
       case 'nuvola':
-        url = '/nuvola' + distro_spec + '/'
+        url = '/nuvola' + distroSpec + '/'
         break
       default:
-        url = '/app/' + target + distro_spec + '/'
+        url = '/app/' + target + distroSpec + '/'
         break
     }
     window.location.href = url
   }
 
   checkHash()
-  var PLATFORM_X64 = ['x86_64', 'x86-64'];
+
+  var PLATFORM_X64 = ['x86_64', 'x86-64']
   var USER_AGENT_X64 = [
     'x86_64', 'x86-64', 'Win64', 'x64;', 'amd64', 'AMD64', 'WOW64', 'x64_64'
   ]
 
   var isPlatformAmd64 = function () {
-    var ua = navigator.userAgent;
-    var oscpu = window.navigator.oscpu || "";
-    var platform = window.navigator.platform || "";
+    var ua = navigator.userAgent
+    var oscpu = window.navigator.oscpu || ''
+    var platform = window.navigator.platform || ''
     for (var i = 0; i < PLATFORM_X64.length; i++) {
-      var test = PLATFORM_X64[i];
+      var test = PLATFORM_X64[i]
       if (platform.indexOf(test) !== -1 || oscpu.indexOf(test) !== -1) {
-        return true;
+        return true
       }
     }
     for (i = 0; i < USER_AGENT_X64.length; i++) {
       if (ua.indexOf(USER_AGENT_X64[i]) !== -1) {
-        return true;
+        return true
       }
     }
-    return false;
+    return false
   }
-  var notAmd64 = !isPlatformAmd64();
+  var notAmd64 = !isPlatformAmd64()
   var elms = window.document.querySelectorAll('.not-amd64')
   for (var i = 0; i < elms.length; i++) {
-    elms[i].style.display = notAmd64 ? "block" : "none"
+    elms[i].style.display = notAmd64 ? 'block' : 'none'
+  }
   }
 })(window)
