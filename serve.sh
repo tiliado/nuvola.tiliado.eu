7ebb23264c5b5b46ef65f252b4d9468254f4db5b
Jiří Janoušek <janousek.jiri@gmail.com>
2018-08-03 23:56:58 +0200

    serve.sh: automatically kill webserver
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- serve.sh
+++ serve.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env sh
 set -eu
 WD="$PWD"
+PID=0
+
+finish() {
+    if test ${PID}; then kill ${PID}; PID=0; fi
+}
+trap finish EXIT INT
+
 while true
 do
     cd "$WD"
-    python3.6 -m nuvola_index -f 'http://127.0.0.1:8000/'
+    python3.6 -m nuvola_index -f 'http://127.0.0.1:8001/'
     cd build
-    python3 -m http.server &
+    python3 -m http.server 8001 --bind 127.0.0.1 &
     PID=$!
     inotifywait -e modify -e move -e create -e delete -e close_write -e attrib -r "$WD" "$WD/fxwebgen/"
     kill ${PID}
+    PID=0
 done
